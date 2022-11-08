fab0bbdd605a0d7e084a3d995e5fc0ede731ddbe
Ivan <synergyivanb@gmail.com>
2022-11-08 20:53:01 +0200

    Movement(Optimized(maybe))

---
+++ Assets/Scripts/Act 1/Movement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public KeyCode ForwardButton;
+    public KeyCode BackwardButton;
+    public KeyCode RightButton;
+    public KeyCode LeftButton;
+
+    private Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+    void Update()
+    {
+        anim.SetBool("GoForward", Input.GetKey(ForwardButton));
+        anim.SetBool("GoRight", Input.GetKey(RightButton));
+        anim.SetBool("GoLeft", Input.GetKey(LeftButton));
+        anim.SetBool("GoBackward", Input.GetKey(BackwardButton));
+    }
+}
