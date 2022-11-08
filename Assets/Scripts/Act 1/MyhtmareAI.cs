7b6a19f878860b34eac0b72c316f88120cfdaf9f
Ivan <synergyivanb@gmail.com>
2022-11-09 00:17:03 +0200

    AI + Myhtmare(The Doll) stages
    Added AI with func of door, closet (Needed objects themselfs) and window(full workable). Added window stages for Myhtmare

---
+++ Assets/Scripts/Act 1/MyhtmareAI.cs
@@ -0,0 +1,224 @@
+using UnityEngine;
+
+public class MyhtmareAI : MonoBehaviour
+{
+    #region [Progresses]
+    public float WindowProgress { get; private set; }
+    public float DoorProgress { get; private set; }
+    public float ClosetProgress { get; private set; }
+    #endregion
+
+    #region [Delays]
+    private bool windowDelayed = false;
+    private bool doorDelayed = false;
+    private bool closetDelayed = false;
+    #endregion
+
+    #region [Timers]
+    private float windowTimeLeft = 2f;
+    private bool windowTimerIsActive = false;
+
+    private float inRoomTimeLeft = 6.5f;
+    private bool isInRoom = false;
+
+    private float closetTimeLeft = 2f;
+    private bool closetTimerIsActive = false;
+    #endregion
+
+    #region [Spots]
+    [SerializeField] private WindowScript Window;
+    //Needed closet here
+    //Needed door here
+    #endregion
+
+    private float calmModifier = 1f;
+    private float progressLimit = 100f;
+    private float spotsCDByDoor = 5.5f;
+    private float roomEnterDelay = 2.25f;
+
+    private void Start()
+    {
+        TryBlock();
+    }
+    private void Update()
+    {
+        TryIncreaseProgress();
+
+        ClampValues();
+
+        CheckProgress();
+
+        TryStartTimersCountdown();
+    }
+    private void CheckProgress()
+    {
+        //if(DoorProgress >= progressLimit)
+        //{
+            //isInRoom = true;
+
+            //ChangeMyhtmareSpots(true, true, false);
+
+            //Invoke(nameof(EnterRoom), roomEnterDelay);
+
+            //Invoke(nameof(UnblockMyhtmare), spotsCDByDoor);
+        //}
+        if(WindowProgress >= progressLimit && !isInRoom)
+        {
+            WindowJumpscare();
+        }
+        if(ClosetProgress >= progressLimit && !isInRoom)
+        {
+            //ClosetJumpscare();
+        }
+    }
+    #region [Jumpscares]
+    private void EnterRoom()
+    {
+        if(inRoomTimeLeft <= 0f) // and player is not sleeping
+        {
+            //do jumpscare
+        }
+        else if (inRoomTimeLeft <= 0) // and player is sleeping
+        {
+            //leave
+            DoorProgress = 0f;
+
+            isInRoom = false;
+            inRoomTimeLeft = 6.5f;
+
+            calmModifier -= 1f;
+        }
+    }
+    private void WindowJumpscare()
+    {
+        windowTimerIsActive = true;
+
+        if(windowTimeLeft <= 0f && !Window.IsClosed)
+        {
+            //do jumpscare
+            Debug.Log("Window jumscare");
+        }
+        else if (windowTimeLeft <= 0 && Window.IsClosed)
+        {
+            WindowProgress = 0f;
+
+            windowTimerIsActive = false;
+            windowTimeLeft = 2f;
+
+            calmModifier -= 1.5f;
+        }
+    }
+    private void ClosetJumpscare()
+    {
+        closetTimerIsActive = true;
+
+        if (closetTimeLeft <= 0) // and Closet is NOT closed
+        {
+            //do jumscare
+        }
+        else if (closetTimeLeft <= 0) // and Closte IS closed
+        {
+            ClosetProgress = 0f;
+
+            closetTimerIsActive = false;
+            closetTimeLeft = 2f;
+
+            calmModifier -= 2f;
+        }
+    }
+    #endregion
+    private void ClampValues()
+    {
+        WindowProgress = Mathf.Clamp(WindowProgress, 0f, 100f);
+        DoorProgress = Mathf.Clamp(DoorProgress, 0f, 100f);
+        ClosetProgress = Mathf.Clamp(ClosetProgress, 0f, 100f);
+    }
+    private void TryBlock()
+    {
+        float chanceToBlock = Random.value;
+
+        if(chanceToBlock <= 0.15f)
+        {
+            ChangeMyhtmareSpots(true, true, true);
+
+            Invoke(nameof(UnblockMyhtmare), 5f);
+
+            return;
+        }
+
+        if(chanceToBlock <= 0.35f)
+        {
+            ChangeMyhtmareSpots(true, true, false);
+
+            Invoke(nameof(UnblockMyhtmare), 5f);
+
+            return;
+        }
+
+        if (chanceToBlock <= 0.55f)
+        {
+            ChangeMyhtmareSpots(true, false, false);
+
+            Invoke(nameof(UnblockMyhtmare), 5f);
+
+            return;         
+        }
+
+        if (chanceToBlock <= 0.75f)
+        {
+            ChangeMyhtmareSpots(false, true, false);
+
+            Invoke(nameof(UnblockMyhtmare), 5f);
+
+            return;
+        }
+
+        ChangeMyhtmareSpots(false, false, false);
+
+        Invoke(nameof(UnblockMyhtmare), 5f);
+    }
+    private void TryStartTimersCountdown()
+    {
+        if(windowTimerIsActive)
+        {
+            windowTimeLeft -= Time.deltaTime;
+        }
+        if(closetTimerIsActive)
+        {
+            closetTimeLeft -= Time.deltaTime;
+        }
+        if(isInRoom)
+        {
+            inRoomTimeLeft -= Time.deltaTime;
+        }
+    }
+    private void ChangeMyhtmareSpots(bool window, bool closet, bool door)
+    {
+        windowDelayed = window;
+        closetDelayed = closet;
+        doorDelayed = door;
+    }
+    private void UnblockMyhtmare()
+    {
+        ChangeMyhtmareSpots(false, false, false);
+    }
+    private void TryIncreaseProgress()
+    {
+        if (!windowDelayed)
+        {
+            WindowProgress += RandomIncreasement();
+        }
+        if (!closetDelayed)
+        {
+            ClosetProgress += RandomIncreasement();
+        }
+        if (!doorDelayed)
+        {
+            DoorProgress += RandomIncreasement();
+        }
+    }
+    private float RandomIncreasement()
+    {
+        return (Time.deltaTime * Random.value * 10f) / calmModifier;
+    }
+}
---
+++ Assets/Scripts/Act 1/WindowStages.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+
+public class WindowStages : MonoBehaviour
+{
+    [SerializeField] private MyhtmareAI AI;
+
+    [SerializeField] private Transform Stage0;
+    [SerializeField] private Transform Stage1;
+    [SerializeField] private Transform Stage2;
+    [SerializeField] private Transform Stage3;
+
+    [SerializeField] private GameObject MyhtmareTransform;
+
+    private int level;
+
+    private void Update()
+    {
+        GetLevel();
+
+        SetPosition();
+    }
+    private void GetLevel()
+    {
+        if(AI.WindowProgress >= 85f)
+        {
+            level = 3;
+            return;
+        }
+        if(AI.WindowProgress >= 45.5f)
+        {
+            level = 2;
+            return;
+        }
+        if(AI.WindowProgress >= 12.5f)
+        {
+            level = 1;
+            return;
+        }
+
+        level = 0;
+    }
+    private void SetPosition()
+    {
+        switch (level)
+        {
+            case 0:
+                MyhtmareTransform.transform.position = Stage0.position;
+                break;
+            case 1:
+                MyhtmareTransform.transform.position = Stage1.position;
+                break;
+            case 2:
+                MyhtmareTransform.transform.position = Stage2.position;
+                break;
+            case 3:
+                MyhtmareTransform.transform.position = Stage3.position;
+                break;
+        }
+    }
+}
