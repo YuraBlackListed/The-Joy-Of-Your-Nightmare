6dad7c5175b16820cafc69d04d822a300478f309
Ivan <synergyivanb@gmail.com>
2022-11-15 14:09:28 +0200

    Added Journal/Taskbook (Quests only for now)

---
+++ Assets/Scripts/QuestWindowScript.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using TMPro;
+
+public class QuestWindowScript : MonoBehaviour
+{
+    [SerializeField] private TMP_Text ThisTitle;
+    [SerializeField] private TMP_Text ThisDescription;
+
+    [SerializeField] private GameObject MainQuestIcon;
+    [SerializeField] private GameObject SideQuestIcon;
+    [SerializeField] private GameObject TipIcon;
+
+    public void CreateQuestWindow(string title, string desc, QuestType type)
+    {
+        ThisTitle.text = title;
+        ThisDescription.text = desc;
+
+        SetIcon(type);
+    }
+    private void SetIcon(QuestType type)
+    {
+        switch (type)
+        {
+            case QuestType.MainQuest:
+                MainQuestIcon.SetActive(true);
+                break;
+            case QuestType.SideQuest:
+                SideQuestIcon.SetActive(true);
+                break;
+            case QuestType.Tip:
+                TipIcon.SetActive(true);
+                break;
+        }
+    }
+}
